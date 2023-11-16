6074e7ea7842e54efa2ba05e8e362ba3d45c308e
Siddharth Agnihotri <siddharthaagnihotri436@gmail.com>
2023-11-16 11:49:01 +0530

    config file changes

--- next.config.js
+++ next.config.js
@@ -4,13 +4,14 @@ const nextConfig = {
         images: false, // or configure as needed
     },
 
-    // Use generateStaticParams() instead of exportPathMap
-    generateStaticParams: async () => {
-        return {
-            '/': { page: '/' },
-            // Add other routes as needed
-        };
+    // Use generateBuildId instead of generateStaticParams
+    generateBuildId: async () => {
+        // Return a unique id for caching purposes
+        return 'my-build-id';
     },
+
+    // Optional: Define the output directory for the exported files
+    distDir: 'out',
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
