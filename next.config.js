65f3ed429e7067dc84ecf41082591cc9d02b0292
Siddharth Agnihotri <siddharthaagnihotri436@gmail.com>
2023-11-16 11:52:22 +0530

    config file changes again

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    experimental: {
-        images: false, // or configure as needed
-    },
-
-    // Use generateBuildId instead of generateStaticParams
-    generateBuildId: async () => {
-        // Return a unique id for caching purposes
-        return 'my-build-id';
-    },
-
-    // Optional: Define the output directory for the exported files
-    distDir: 'out',
-}
-
-module.exports = nextConfig;
+    output: 'export',
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
