c5a5d28f88c53e1a5e8356ee076792ade7f21f8b
Siddharth Agnihotri <siddharthaagnihotri436@gmail.com>
2023-11-16 11:55:48 +0530

    config file changed

--- next.config.js
+++ next.config.js
@@ -3,15 +3,26 @@
  */
 const nextConfig = {
     output: 'export',
-   
+
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
-   
+
     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
     // skipTrailingSlashRedirect: true,
-   
+
     // Optional: Change the output directory `out` -> `dist`
     // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+    experimental: {
+        images: false, // or configure as needed
+    },
+
+    // Use generateStaticParams() instead of exportPathMap
+    generateStaticParams: async () => {
+        return {
+            '/': { page: '/' },
+            // Add other routes as needed
+        };
+    },
+}
+
+module.exports = nextConfig
